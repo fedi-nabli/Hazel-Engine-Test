8a92729343fcb309b5bc9e6a1ca671ee20027b5f
fedinabli05 <fedi.nabli123@gmail.com>
2021-10-25 22:17:33 +0100

    Setup basic Application and Entry Points

---
+++ Hazel/src/Hazel.h
@@ -0,0 +1,9 @@
+#pragma once
+
+//For use by Hazel applications
+
+#include "Hazel/Application.h"
+
+// ---Enrty Point------------------
+#include "Hazel/EntryPoint.h"
+// --------------------------------
