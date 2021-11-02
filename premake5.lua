72b48f6117f6abb7c2c0d877429bbf5154b73076
fedinabli05 <fedi.nabli123@gmail.com>
2021-11-02 20:34:48 +0100

    Added layers and layer stack

--- premake5.lua
+++ premake5.lua
@@ -58,15 +58,18 @@ project "Hazel"
 
 	filter "configurations:Debug"
 		defines "HZ_DEBUG"
+		buildoptions "/MDd"
 		symbols "On"
 	
 
 	filter "configurations:Release"
 		defines "HZ_RELEASE"
+		buildoptions "/MDd"
 		optimize "On"
 
 	filter "configurations:Dist"
 		defines "HZ_DIST"
+		buildoptions "/MDd"
 		optimize "On"
 
 project "Sandbox"
@@ -101,12 +104,15 @@ project "Sandbox"
 
 	filter "configurations:Debug"
 		defines "HZ_DEBUG"
+		buildoptions "/MDd"
 		symbols "On"
 
 	filter "configurations:Release"
 		defines "HZ_RELEASE"
+		buildoptions "/MDd"
 		optimize "On"
 
 	filter "configurations:Dist"
 		defines "HZ_DIST"
+		buildoptions "/MDd"
 		optimize "On"
