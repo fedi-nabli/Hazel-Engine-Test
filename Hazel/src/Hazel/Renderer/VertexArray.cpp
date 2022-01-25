429e9f34c2b4b0e1fb4ab240c8f4bb5b1ab5d530
fedi-nabli <fedinabli@gmail.com>
2022-01-26 00:41:01 +0100

    Added vertex array support

---
+++ Hazel/src/Hazel/Renderer/VertexArray.cpp
@@ -0,0 +1,17 @@
+#include "hzpch.h"
+#include "VertexArray.h"
+
+#include "Renderer.h"
+#include "Platform/OpenGL/OpenGLVertexArray.h"
+
+namespace Hazel {
+	VertexArray* VertexArray::Create() {
+		switch (Renderer::GetAPI()) {
+			case RendererAPI::None: HZ_CORE_ASSERT(false, "RendererAPI::NONE is currently not supported!"); return nullptr;
+			case RendererAPI::OpenGL: return new OpenGLVertexArray();
+		}
+
+		HZ_CORE_ASSERT(false, "Unknown RendererAPI!");
+		return nullptr;
+	}
+}
